06ada20e4efb36f4b0272ec7bb25b52d42d281f7
Jaque Meira <jaquemeira7@gmail.com>
2022-05-15 20:57:38 -0300

    atualizacoes

--- src/test/java/br/com/banco/bajax/steps/ClienteSteps.java
+++ src/test/java/br/com/banco/bajax/steps/ClienteSteps.java
@@ -1,10 +1,13 @@
 package br.com.banco.bajax.steps;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import java.util.Map;
 
 import br.com.jaque.banco.bajax.entities.Cliente;
+import br.com.jaque.banco.bajax.services.ClienteService;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
@@ -17,18 +20,23 @@ public class ClienteSteps {
 	String cpf;
 	boolean seAtivo;
 	Cliente cliente;
-	
+	List<String> clientes;
+	ClienteService cs;
+	List<Map<String, String>> linhas;
+
 	@Dado("que possuo as informações válidas de um individuo")
 	public void quePossuoAsInformaçõesVálidasDeUmIndividuo() {
 		nome = "Yuri";
 		email = "yuri@gmail.com";
 		cpf = "123456789-11";
 		seAtivo = true;
+		System.out.println("teste lalalala");
 	}
 
 	@Quando("crio um cliente com essas informações")
 	public void crioUmClienteComEssasInformações() {
 		cliente = new Cliente(nome, email, cpf, seAtivo);
+		assertNull(cliente);
 	}
 
 	@Então("o cliente é cadastrado com sucesso")
@@ -41,51 +49,46 @@ public class ClienteSteps {
 
 	@Dado("que possuo as informações para criar um cliente a partir da lista")
 	public void quePossuoAsInformaçõesParaCriarUmClienteAPartirDaLista(DataTable dataTable) {
-	
-		List<String> clientes = dataTable.asList();
+
+		List<List<String>> rows = dataTable.asLists(String.class);
+		
+		for(List<String> row : rows) {
+			System.out.println(row.get(0));
+		}
+		
+		
+		
+		
+		
+		clientes = dataTable.asList();
 		System.out.println(clientes);
+
+		linhas = dataTable.asMaps(String.class, String.class);
+
+		for (Map<String, String> colunas : linhas) {
+			Cliente c1 = new Cliente(colunas.get("nome"), colunas.get("email"), colunas.get("cpf"),
+					Boolean.valueOf(colunas.get("ativo")));
+			assertEquals(c1.getNome(), "1");
+		}
+
 	}
 
-	@Quando("adiciono o {string} vazio")
-	public void adicionoOVazio(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@Quando("adiciono um campo como nulo")
+	public void adicionoUmCampoComoNulo(io.cucumber.datatable.DataTable dataTable) {
+		
+		linhas = dataTable.asMaps(String.class, String.class);
+		for (Map<String, String> colunas : linhas) {
+			Cliente c2 = new Cliente(colunas.get("nome"), colunas.get("email"), colunas.get("cpf"),
+					Boolean.valueOf(colunas.get("ativo")));
+		}
+		
 	}
 
-	@Então("ao tentar criar o cliente recebo a mensagem de erro {string}")
-	public void aoTentarCriarOClienteReceboAMensagemDeErro(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Quando("adiciono o {string} no formato inválido")
-	public void adicionoONoFormatoInválido(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Dado("que tenho um cliente válido")
-	public void queTenhoUmClienteVálido() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Quando("adiciono um telefone no formato inválido")
-	public void adicionoUmTelefoneNoFormatoInválido() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Então("recebo uma mensagem de erro")
-	public void receboUmaMensagemDeErro() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Quando("tento alterar o campo ativo para nulo")
-	public void tentoAlterarOCampoAtivoParaNulo() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@Então("ao tentar criar o cliente recebo uma mensagem de erro")
+	public void aoTentarCriarOClienteReceboUmaMensagemDeErro() {
+		
+		
+	    System.out.println("jaque");
 	}
 
 }
--- target/test-classes/features/cadastrar_cliente.feature
+++ target/test-classes/features/cadastrar_cliente.feature
@@ -13,41 +13,10 @@ Cenário: deve criar cliente com sucesso
 
 Cenário: cliente inválido campos obrigatórios nulos
 	Dado que possuo as informações para criar um cliente a partir da lista
-	| Yuri Azul | yuri@gmail.com | 123456789-11 | true |
-	# Quando adiciono o "<campo>" como nulo
-	# Então ao tentar criar o cliente recebo a mensagem de erro "<mensagem>"
-
-Esquema do Cenário: cliente inválido campos obrigatórios vazios
-	Dado que possuo as informações válidas de um individuo
-	Quando adiciono o "<campo>" vazio
-	Então ao tentar criar o cliente recebo a mensagem de erro "<mensagem>"
-	
-Exemplos:
-	|     campo      |       mensagem       |
-	|     nome       |   Dados inválidos!   |
-	|     email      |   Dados inválidos!   |
-	|     cpf        |   Dados inválidos!   |
-	|     ativo      |   Dados inválidos!   |
-
-Esquema do Cenário: cliente inválido campos obrigatórios no formato inválido
-	Dado que possuo as informações válidas de um individuo
-	Quando adiciono o "<campo>" no formato inválido
-	Então ao tentar criar o cliente recebo a mensagem de erro "<mensagem>"
-	
-Exemplos:
-	|     campo      |       mensagem       |
-	|     nome       |   Dados inválidos!   |
-	|     email      |   Dados inválidos!   |
-	|     cpf        |   Dados inválidos!   |
-	|     ativo      |   Dados inválidos!   |
-
-Cenário: adicionar telefone no formato inválido
-	Dado que tenho um cliente válido
-	Quando adiciono um telefone no formato inválido
-	Então recebo uma mensagem de erro
-
-Cenário: alterar campo ativo para nulo
-	Dado que tenho um cliente válido
-	Quando tento alterar o campo ativo para nulo
-	Então recebo uma mensagem de erro
+	| nome      | email          | cpf          | ativo   |
+	| Yuri Azul | yuri@gmail.com | 123456789-11 | true    |
+	Quando adiciono um campo como nulo
+	| nome      | email          | cpf          | ativo   |
+	|           | yuri@gmail.com | 123456789-11 | true    |
+	Então ao tentar criar o cliente recebo uma mensagem de erro
   
