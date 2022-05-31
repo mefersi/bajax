8ecc3c6bce78f8822b1324c5874f64cda05a4371
mefersi <61852203+mefersi@users.noreply.github.com>
2022-05-31 19:59:38 -0300

    Delete src directory

--- src/main/java/br/com/jaque/banco/bajax/entities/DadosInvalidosException.java
+++
@@ -1,9 +0,0 @@
-package br.com.jaque.banco.bajax.entities;
-
-public class DadosInvalidosException extends RuntimeException {
-	private static final long serialVersionUID = 1169378003599229797L;
-
-	public DadosInvalidosException(String msg) {
-		super(msg);
-	}
-}
